8c0416113d649db186376c72149ea2dbb3fac22f
cdthomp1 <camthomp96@gmail.com>
2022-03-01 21:38:34 -0700

    init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const puppeteer = require('puppeteer')
+let state;
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+export default async (req, res) => {
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://nytimes.com/games/wordle/index.html');
+
+    const localStorage = await page.evaluate(() => Object.assign({}, window.localStorage));
+    state = JSON.parse(localStorage['nyt-wordle-state']);
+
+    await browser.close();
+  
+  res.status(200).json({ wordleSolution: state.solution })
 }
